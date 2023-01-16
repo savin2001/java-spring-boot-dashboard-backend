1802096bde07bb061ecb0ea7c4c62e00ff232eb2
Savin Osuka <osukasavin2001@gmail.com>
2023-01-16 12:08:55 +0300

    Update and delete functionality added.

--- src/main/java/com/osukatech/adminDashboard/EmployeeController/EmployeeController.java
+++ src/main/java/com/osukatech/adminDashboard/EmployeeController/EmployeeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("api/v1/employee")
 public class EmployeeController {
     @Autowired
@@ -24,7 +24,7 @@ public class EmployeeController {
     }
 
     //  List of the employees
-    @GetMapping(path = "/get-all-customers")
+    @GetMapping(path = "/get-all-employees")
     public List<EmployeeDTO> getAllEmployees() {
         List<EmployeeDTO> allEmployees = employeeService.getAllEmployees();
         return allEmployees;
